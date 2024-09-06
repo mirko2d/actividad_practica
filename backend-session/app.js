38b62fdb95fe1591113add4c85da355b01bdac81
mirko2de <mirkoacosta55@gmail.com>
2024-09-06 10:15:40 -0300

    se modulario el fronted

--- backend-session/app.js
+++ backend-session/app.js
@@ -34,50 +34,5 @@ app.use(session({
         // sameSite: 'lax' // permite envío de cookies en navegadores modernos
     }
 }));
-
-
-
-// Ruta para manejar el inicio de sesión
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    // Buscar usuario
-    const user = connection.user.find(u => u.username === username && u.password === password);
-
-    if (user) {
-        // Guardar información del usuario en la sesión
-        req.session.userId = user.id;
-        req.session.username = user.username;
-
-        return res.json({ 
-            message: 'Inicio de sesión exitoso', 
-            user: { id: user.id, username: user.username } });
-    } else {
-        return res.status(401).json({ message: 'Credenciales incorrectas' });
-    }
-});
-
-// Ruta para obtener los datos de la sesión
-app.get('/session', (req, res) => {
-    if (req.session.userId) {
-        return res.json({ 
-            loggedIn: true, 
-            user: { id: req.session.userId, username: req.session.username } });
-    } else {
-        return res.status(401).json({ loggedIn: false, message: 'No hay sesión activa' });
-    }
-});
-
-// Ruta para cerrar la sesión
-app.post('/logout', (req, res) => {
-    console.log(req.session)
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).json({ message: 'Error al cerrar la sesión' });
-        }
-        res.clearCookie('connect.sid'); // Nombre de cookie por defecto para express-session
-        return res.json({ message: 'Sesión cerrada exitosamente' });
-    });
-});
-
+app.use(connection) 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
